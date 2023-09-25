056cb67008eebab94b6850a73ecb1ee10d760aa3
diry86 <suetin.dmitry@gmail.com>
2023-09-25 13:12:18 +1000

    update metod and test

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -13,7 +13,7 @@ public class Welcomer{
 	public String saySome(){
 		return "something in the way";
 	}
-//	public String sayTest(){
-//		return "I'm not a hunter";
-//	}
+	public String sayTest(){
+		return "I'm not a hunter";
+	}
 }
